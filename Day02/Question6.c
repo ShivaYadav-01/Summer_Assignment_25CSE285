2850484eb50eebb742bdae31113d60829aafd3a0
ShivaYadav-01 <shiva.yadav2025@glbajajgroup.org>
2026-07-14 08:29:57 +0530

    Add files via upload

---
+++ Day02/Question6.c
@@ -0,0 +1,17 @@
+// Program to reverse a number
+#include <stdio.h>
+
+int main() {
+    int n, rev = 0, rem;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+    
+    while(n != 0) {
+        rem = n % 10;
+        rev = rev * 10 + rem;
+        n /= 10;
+    }
+    
+    printf("Reversed number is: %d\n", rev);
+    return 0;
+}
